b11e57f6eb9fba6db3497b444d659ef81f16840e
NovaZHub <carlosfrancisquini02@gmail.com>
2026-01-11 20:53:59 -0300

    Atualizar o Hitboxexpand.lua

--- Hitboxexpand.lua
+++ Hitboxexpand.lua
@@ -1,7 +1,7 @@
 loadstring([[
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/Feedbackbot.lua"))()
-    task.wait(4)
+    task.wait(2)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/Suggestions.lua", true))()
-    task.wait(1)
+    task.wait(0)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/NovaZHubOFC.lua"))()
 ]])()
