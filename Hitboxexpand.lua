b6212ccd0653e7b474295c0f6cb7f3e5619caa68
NovaZHub <carlosfrancisquini02@gmail.com>
2026-01-14 01:15:48 -0300

    Atualizar o Hitboxexpand.lua

--- Hitboxexpand.lua
+++ Hitboxexpand.lua
@@ -1,9 +1,10 @@
-loadstring([[
- loadstring(game:HttpGet("https://github.com/NovaZHubOFC/NovaZHubOFC/blob/main/DoyouwantanofficialDiscordserver?.lua"))()
-    task.wait(5)
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/Feedbackbot.lua"))()
-    task.wait(2)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/Suggestions.lua", true))()
-    task.wait(0)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/NovaZHubOFC.lua"))()
-]])()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/DoyouwantanofficialDiscordserver%3F.lua"))()
+task.wait(5)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/Feedbackbot.lua"))()
+task.wait(2)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/Suggestions.lua"))()
+task.wait()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NovaZHubOFC/NovaZHubOFC/main/NovaZHubOFC.lua"))()
